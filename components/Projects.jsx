be74f8e1ee6731c53b96ad7611d3591ab4f4b4fb
Vincentsarsah <vincent.sarsah@amalitech.org>
2023-01-21 20:47:48 +0000

    Update Projects.jsx

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -77,12 +77,12 @@ const Projects = () => {
             projectUrl="/scrappy_consult"
             tech="React"
           />
-          <ProjectItem
+       {/*    <ProjectItem
             title="Cleaning Services"
             backgroundImg={Shine}
             projectUrl="/shine_cleaning_services"
             tech="Next JS"
-          />
+          /> */}
         </div>
       </div>
     </div>
