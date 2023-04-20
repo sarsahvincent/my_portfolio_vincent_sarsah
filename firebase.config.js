65ec0c2f3b69fa2d007a1fd8c4995604d131d032
Vincent Andoh Sarsah <vincent.sarsah@amalitech.com>
2023-04-20 20:54:24 +0000

    update

--- firebase.config.js
+++ firebase.config.js
@@ -9,7 +9,7 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey:"AIzaSyA8HlvGuk6SZ8x-RunQyN6hNOrA8HPRv9Y",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
